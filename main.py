3c513c6ef2afe94803d1f7af7e7524ffc4896280
Andrew Taylor <andrewtaylorcode@gmail.com>
2025-12-22 09:34:00 -0600

    Pass in content dynamically

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    args = parser.parse_args()
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=args.user_prompt)
+    if response.usage_metadata == None:
+        raise RuntimeError("Response came back empty. Try again?")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
